d8a587d3b05d5971acd218cb70957c4b4d2c8784
cat-lover-of-doom <wist.sebastian@gmail.com>
2023-09-10 15:53:13 -0500

    bromis

--- test.bat
+++ test.bat
@@ -1,7 +1,6 @@
 del .2.bat
 @echo off
 :loop
-echo night_edit
-echo edit_from_lp_tp
-echo hohohoho
+echo ERROR directorio: /usr/waitman/win32: el archivo no existe, lanzando herramientas de reparacion ...
+echo  "at org.mortbay.jetty.servlet.ServletHandlerCachedChain.doFilter(ServletHandler.java:1157\nat org.mortbay.jetty.servlet.ServletHandler.handle(ServletHandler.java:388\nat org.mortbay.jetty.security.SecurityHandler.handle(SecurityHandler.java:216\nat org.mortbay.jetty.servlet.SessionHandler.handle(SessionHandler.java:182\nat org.mortbay.jetty.handler.ContextHandler.handle(ContextHandler.java:765\nat org.mortbay.jetty.webapp.WebAppContext.handle(WebAppContext.java:418\nat org.mortbay.jetty.handler.HandlerWrapper.handle(HandlerWrapper.java:152\nat org.mortbay.jetty.Server.handle(Server.java:326\nat org.mortbay.jetty.HttpConnection.handleRequest(HttpConnection.java:542\nat org.mortbay.jetty.HttpConnection$RequestHandler.content(HttpConnection.java:943\nat org.mortbay.jetty.HttpParser.parseNext(HttpParser.java:756\nat org.mortbay.jetty.HttpParser.parseAvailable(HttpParser.java:218\nat org.mortbay.jetty.HttpConnection.handle(HttpConnection.java:404\nat org.mortbay.jetty.bio.SocketConnectorConnection.run(SocketConnector.java:228\nat org.mortbay.thread.QueuedThreadPool$PoolThread.run(QueuedThreadPool.java:582\nCaused by: com.example.myproject.MyProjectServletException"
 goto loop
